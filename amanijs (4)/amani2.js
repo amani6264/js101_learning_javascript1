3774a2f201aaf17f409dd999ed228a15be662ce4
amani6264 <gamani6264@gmail.com>
2023-01-01 17:42:59 +0530

    first commit

---
+++ amanijs (4)/amani2.js
@@ -0,0 +1,6 @@
+let name="amani satya gadi";
+console.log(name);
+let fathername="satyannarayana gadi";
+console.log(fathername);
+let mothername="ramalakshmi gadi";
+console.log(mothername);
---
+++ amanijs (4)/amani3.js
@@ -0,0 +1,4 @@
+let name="amani satya";
+let age="23";
+console.log(name);
+console.log(age);
